fd6ca17de7cbafbd16da419ea5ef96316c94db27
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-12-26 13:33:57 +0100

    Started integrating existing modules.

--- src/TipExpert.Net/Controllers/HomeController.cs
+++ src/TipExpert.Net/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Mvc;
 
 namespace TipExpert.Net.Controllers
 {
@@ -10,6 +6,7 @@ namespace TipExpert.Net.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Titel = "Test";
             return View();
         }
 
--- src/TipExpert.Net/wwwroot/js/app.js
+++ src/TipExpert.Net/wwwroot/js/app.js
@@ -1,12 +1,11 @@
 'use strict';
 
-// define all available modules
-//angular.module('tipExpert.user', [ 'ngCookies' ]);
-//angular.module('tipExpert.match', [ ]);
-//angular.module('tipExpert.game', [ ]);
+angular.module("tipExpert.home", []);
 
-var tipExpert = angular.module('tipExpert', ['tipExpert.home']);//, 'tipExpert.user', 'tipExpert.match', 'tipExpert.game',
-                                             //'ui.bootstrap', 'ui.router', 'ngRoute', 'pascalprecht.translate']);
+var tipExpert = angular.module('tipExpert', ['tipExpert.home', 'ui.bootstrap', 'ui.router', 'pascalprecht.translate']);
+
+            //, 'tipExpert.user', 'tipExpert.match', 'tipExpert.game',
+            //'ui.bootstrap', 'ui.router', 'ngRoute', 'pascalprecht.translate']);
 
 // configure the main module
 tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
@@ -14,7 +13,7 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
     var accessLevels = userConfig.accessLevels;
     var abstractView = {
         'header': {
-            templateUrl: '/modules/home/views/header.html',
+            templateUrl: '/js/home/views/header.html',
             controller: 'navigationController',
         },
         'main': {
@@ -35,14 +34,14 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
         .state('user.overview', {
             title: 'Users',
             url: '',
-            templateUrl: '/modules/user/views/user.html',
+            templateUrl: '/js/user/views/user.html',
             controller: 'userController',
             access: accessLevels.user
         })
         .state('user.profile', {
             title: 'User profile',
             url: '/:userId',
-            templateUrl: '/modules/user/views/profile.html',
+            templateUrl: '/js/user/views/profile.html',
             controller: 'userProfileController',
             access: accessLevels.user
         })
@@ -56,7 +55,7 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
         .state('matches.overview', {
             title: 'Matches',
             url: '',
-            templateUrl: '/modules/match/views/matches.html',
+            templateUrl: '/js/match/views/matches.html',
             controller: 'matchController',
             access: accessLevels.admin
         })
@@ -77,35 +76,35 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
         .state('games.overview', {
             title: 'Games',
             url: '',
-            templateUrl: '/modules/game/views/myGames.html',
+            templateUrl: '/js/game/views/myGames.html',
             controller: 'myGamesController',
             access: accessLevels.user // todo
         })
         .state('games.history', {
             title: 'Games History',
             url: '/history',
-            templateUrl: '/modules/game/views/gamesHistory.html',
+            templateUrl: '/js/game/views/gamesHistory.html',
             controller: 'gamesHistoryController',
             access: accessLevels.user // todo
         })
         .state('games.create', {
             title: 'Create game',
             url: '/create',
-            templateUrl: '/modules/game/views/editGame.html',
+            templateUrl: '/js/game/views/editGame.html',
             controller: 'editGameController',
             access: accessLevels.user // todo
         })
         .state('games.edit', {
             title: 'Edit game',
             url: '/:gameId/edit',
-            templateUrl: '/modules/game/views/editGame.html',
+            templateUrl: '/js/game/views/editGame.html',
             controller: 'editGameController',
             access: accessLevels.user // todo
         })
         .state('games.show', {
             title: 'Game',
             url: '/:gameId',
-            templateUrl: '/modules/game/views/game.html',
+            templateUrl: '/js/game/views/game.html',
             controller: 'gameController',
             access: accessLevels.user // todo
         })
@@ -116,11 +115,11 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
             url: '/',
             views: {
                 'header': {
-                    templateUrl: '/modules/home/views/loginHeader.html',
+                    templateUrl: '/js/home/views/loginHeader.html',
                     controller: 'navigationController',
                 },
                 'main': {
-                    templateUrl: '/modules/home/views/index.html',
+                    templateUrl: '/js/home/views/index.html',
                     controller: 'homeController'
                 }
             },
@@ -147,54 +146,54 @@ tipExpert.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '
     }]);
 }]);
 
-tipExpert.config(['$translateProvider', function($translateProvider) {
+//tipExpert.config(['$translateProvider', function($translateProvider) {
+//
+//    $translateProvider
+//        .fallbackLanguage('en')
+//        .registerAvailableLanguageKeys(['en', 'de'], {
+//            'en_US': 'en',
+//            'en_UK': 'en',
+//            'de_DE': 'de',
+//            'de_CH': 'de',
+//            'de_AT': 'de'
+//        });
+//
+//    $translateProvider.useCookieStorage();
+//    $translateProvider.determinePreferredLanguage();
+//
+//    var origin = window.location.origin || window.location.protocol + '//' + window.location.host;
+//
+//    $translateProvider.useStaticFilesLoader({
+//        prefix:  origin + '/locales/',
+//        suffix: '.json'
+//    });
+//}]);
 
-    $translateProvider
-        .fallbackLanguage('en')
-        .registerAvailableLanguageKeys(['en', 'de'], {
-            'en_US': 'en',
-            'en_UK': 'en',
-            'de_DE': 'de',
-            'de_CH': 'de',
-            'de_AT': 'de'
-        });
-
-    $translateProvider.useCookieStorage();
-    $translateProvider.determinePreferredLanguage();
-
-    var origin = window.location.origin || window.location.protocol + '//' + window.location.host;
-
-    $translateProvider.useStaticFilesLoader({
-        prefix:  origin + '/locales/',
-        suffix: '.json'
-    });
-}]);
-
-tipExpert.run(['$rootScope', '$location', '$state', 'Auth', 'alertService', function($rootScope, $location, $state, Auth, alertService) {
-
-    $rootScope.$state = $state;
-
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-
-        var isLoggedIn = Auth.user.isLoggedIn;
-
-        if (!Auth.authorize(toState.access)) {
-
-            event.preventDefault();
-            if (!isLoggedIn) {
-                $state.go('home');
-            }
-            else {
-                if (!fromState.controller)
-                    $state.go('games.overview');
-
-                alertService.error('You are not allowed to access this page');
-            }
-        }
-        else if (toState.name == 'home' && isLoggedIn) {
-
-            event.preventDefault();
-            $state.go('games.overview');
-        }
-    });
-}]);
+//tipExpert.run(['$rootScope', '$location', '$state', 'Auth', 'alertService', function($rootScope, $location, $state, Auth, alertService) {
+//
+//    $rootScope.$state = $state;
+//
+//    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+//
+//        var isLoggedIn = Auth.user.isLoggedIn;
+//
+//        if (!Auth.authorize(toState.access)) {
+//
+//            event.preventDefault();
+//            if (!isLoggedIn) {
+//                $state.go('home');
+//            }
+//            else {
+//                if (!fromState.controller)
+//                    $state.go('games.overview');
+//
+//                alertService.error('You are not allowed to access this page');
+//            }
+//        }
+//        else if (toState.name == 'home' && isLoggedIn) {
+//
+//            event.preventDefault();
+//            $state.go('games.overview');
+//        }
+//    });
+//}]);
