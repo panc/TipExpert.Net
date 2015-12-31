a967a61a473277d6ccc890319fde54ebfd8dd43d
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-12-31 15:24:32 +0100

    Further work.

--- src/TipExpert.Net/wwwroot/js/game/presenter/selectPlayersPresenter.js
+++ src/TipExpert.Net/wwwroot/js/game/presenter/selectPlayersPresenter.js
@@ -2,60 +2,62 @@
 
 var game = angular.module('tipExpert.game');
 
-game.controller('SelectPlayersController', ['$scope', '$modalInstance', 'Auth', 'userService', 'gameService', 'alertService', 'game', function($scope, $modalInstance, Auth, userService, gameService, alertService, game) {
-    
-    var selectedplayers = game.players.slice(0);
-    
-    var areUserEqual = function(user, otherUser) {
-        return user.name == otherUser.name;
-    };
+game.controller('SelectPlayersController', [
+    '$scope', '$modalInstance', 'authService', 'userService', 'gameService', 'alertService', 'game',
+    function($scope, $modalInstance, authService, userService, gameService, alertService, game) {
 
-    $scope.toggleUserSelection = function(user) {
-        user.selected = !user.selected;
+        var selectedplayers = game.players.slice(0);
 
-        if (user.selected) {
-            // if selected add the user to the player-list of the game
-            var container = { user: user };
-            selectedplayers.push(container);
-        }
-        else {
-            // if not selected remove the user from the player-list of the game
-            angular.forEach(selectedplayers, function(player) {
-                
-                if (areUserEqual(user, player.user)) {
-                    var index = selectedplayers.indexOf(player);
-                    selectedplayers.splice(index, 1);
-                }
-            });
-        }
-    };
+        var areUserEqual = function(user, otherUser) {
+            return user.name == otherUser.name;
+        };
 
-    $scope.save = function() {
+        $scope.toggleUserSelection = function(user) {
+            user.selected = !user.selected;
 
-        game.players = selectedplayers;
-
-        gameService.update(game,
-            function(updatedGame) {
-                $modalInstance.close(updatedGame);
-            },
-            alertService.error);
-    };
-
-    $scope.cancel = function() {
-        $modalInstance.dismiss('cancel');
-    };
-
-    userService.loadFriendsForUser(Auth.currentUser)
-        .then(function(users) {
-            $scope.users = users;
-
-            angular.forEach(users, function(user) {
+            if (user.selected) {
+                // if selected add the user to the player-list of the game
+                var container = { user: user };
+                selectedplayers.push(container);
+            } else {
+                // if not selected remove the user from the player-list of the game
                 angular.forEach(selectedplayers, function(player) {
 
-                    if (areUserEqual(user, player.user))
-                        user.selected = true;
+                    if (areUserEqual(user, player.user)) {
+                        var index = selectedplayers.indexOf(player);
+                        selectedplayers.splice(index, 1);
+                    }
                 });
-            });
-        })
-        .catch(alertService.error);
-}]);
+            }
+        };
+
+        $scope.save = function() {
+
+            game.players = selectedplayers;
+
+            gameService.update(game,
+                function(updatedGame) {
+                    $modalInstance.close(updatedGame);
+                },
+                alertService.error);
+        };
+
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        };
+
+        userService.loadFriendsForUser(authService.currentUser)
+            .then(function(users) {
+                $scope.users = users;
+
+                angular.forEach(users, function(user) {
+                    angular.forEach(selectedplayers, function(player) {
+
+                        if (areUserEqual(user, player.user))
+                            user.selected = true;
+                    });
+                });
+            })
+            .catch(alertService.error);
+    }
+]);
