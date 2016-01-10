71bfe5d4034837ebafbd7fadc73c4b56d8396dc5
Christoph Pangerl <christoph.pangerl@gmail.com>
2016-01-10 15:29:03 +0100

    Load relations after a game entity is loaded.

--- src/TipExpert.Core/DataStore/GameStore.cs
+++ src/TipExpert.Core/DataStore/GameStore.cs
@@ -7,10 +7,14 @@ namespace TipExpert.Core
 {
     public class GameStore : IGameStore
     {
+        private readonly IUserStore _userStore;
+        private readonly IMatchStore _matchStore;
         private readonly IMongoCollection<Game> _collection;
 
-        public GameStore(IMongoDatabase database)
+        public GameStore(IMongoDatabase database, IUserStore userStore, IMatchStore matchStore)
         {
+            _userStore = userStore;
+            _matchStore = matchStore;
             _collection = database.GetCollection<Game>("games");
         }
 
@@ -18,6 +22,7 @@ namespace TipExpert.Core
         {
             game.Id = Guid.NewGuid();
             await _collection.InsertOneAsync(game);
+            await _PopulateRelations(game);
         }
 
         public async Task Remove(Game game)
@@ -28,41 +33,85 @@ namespace TipExpert.Core
         public async Task Update(Game game)
         {
             await _collection.ReplaceOneAsync(x => x.Id == game.Id, game);
+            await _PopulateRelations(game);
         }
 
         public async Task<Game[]> GetGamesCreatedByUser(Guid userId)
         {
-            return await _collection
+            var games = await _collection
                 .Find(x => x.CreatorId == userId && x.IsFinished == false)
                 .ToArrayAsync();
+
+            await _PopulateRelations(games);
+
+            return games;
         }
 
         public async Task<Game[]> GetGamesUserIsInvitedTo(Guid userId)
         {
-            return await _collection
+            var games = await _collection
                 .Find(x => !x.IsFinished && x.Players.Any(p => p.UserId == userId))
                 .ToArrayAsync();
+
+            await _PopulateRelations(games);
+
+            return games;
         }
 
         public async Task<Game[]> GetFinishedGames(Guid userId)
         {
-            return await _collection
+            var games = await _collection
                 .Find(x => x.IsFinished && x.Players.Any(p => p.UserId == userId))
                 .ToArrayAsync();
+
+            await _PopulateRelations(games);
+
+            return games;
         }
 
         public async Task<Game[]> GetGamesForMatch(Guid matchId)
         {
-            return await _collection
+            var games = await _collection
                 .Find(x => x.Matches.Any(m => m.MatchId == matchId))
                 .ToArrayAsync();
+
+            await _PopulateRelations(games);
+
+            return games;
         }
 
         public async Task<Game> GetById(Guid id)
         {
-            return await _collection
+            var game = await _collection
                 .Find(x => x.Id == id)
                 .SingleOrDefaultAsync();
+
+            await _PopulateRelations(game);
+
+            return game;
+        }
+
+        private async Task _PopulateRelations(Game[] games)
+        {
+            foreach (var game in games)
+                await _PopulateRelations(game);
+        }
+
+        private async Task _PopulateRelations(Game game)
+        {
+            if (game.Players != null)
+            {
+                foreach (var player in game.Players)
+                    player.User = await _userStore.GetById(player.UserId);
+            }
+
+            if (game.Matches != null)
+            {
+                foreach (var mt in game.Matches)
+                {
+                    mt.Match = await _matchStore.GetById(mt.MatchId);
+                }
+            }
         }
     }
 }
