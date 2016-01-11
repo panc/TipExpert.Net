8c7c457207b610699b31b88e41c92c55b46d67d1
Christoph Pangerl <christoph.pangerl@gmail.com>
2016-01-11 17:57:15 +0100

    Removed unneeded serverside view files.

--- src/TipExpert.Net/Controllers/AccountController.cs
+++ src/TipExpert.Net/Controllers/AccountController.cs
@@ -141,12 +141,14 @@ namespace TipExpert.Net.Controllers
         [HttpPost("[Action]")]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public IActionResult ExternalLogin(string provider, string returnUrl = null)
+        public async Task<IActionResult> ExternalLogin(string provider, string returnUrl = null)
         {
+            return await _Error();
+
             // Request a redirect to the external login provider.
-            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
-            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            return new ChallengeResult(provider, properties);
+//            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
+//            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+//            return new ChallengeResult(provider, properties);
         }
 
         //
@@ -155,32 +157,34 @@ namespace TipExpert.Net.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null)
         {
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                return RedirectToAction(nameof(Login));
-            }
+            return await _Error();
 
-            // Sign in the user with this external login provider if the user already has a login.
-            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false);
-            if (result.Succeeded)
-            {
-                _logger.LogInformation(5, "User logged in with {Name} provider.", info.LoginProvider);
-                return _RedirectToLocal(returnUrl);
-            }
-            if (result.IsLockedOut)
-            {
-                return View("Error");
-            }
-            else
-            {
-                // If the user does not have an account, then ask the user to create an account.
-                ViewData["ReturnUrl"] = returnUrl;
-                ViewData["LoginProvider"] = info.LoginProvider;
-                //var email = info.ExternalPrincipal.FindFirstValue(ClaimTypes.Email);
-                //return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = email });
-                return View("Error");
-            }
+//            var info = await _signInManager.GetExternalLoginInfoAsync();
+//            if (info == null)
+//            {
+//                return RedirectToAction(nameof(Login));
+//            }
+//
+//            // Sign in the user with this external login provider if the user already has a login.
+//            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false);
+//            if (result.Succeeded)
+//            {
+//                _logger.LogInformation(5, "User logged in with {Name} provider.", info.LoginProvider);
+//                return _RedirectToLocal(returnUrl);
+//            }
+//            if (result.IsLockedOut)
+//            {
+//                return View("Error");
+//            }
+//            else
+//            {
+//                // If the user does not have an account, then ask the user to create an account.
+//                ViewData["ReturnUrl"] = returnUrl;
+//                ViewData["LoginProvider"] = info.LoginProvider;
+//                //var email = info.ExternalPrincipal.FindFirstValue(ClaimTypes.Email);
+//                //return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = email });
+//                return View("Error");
+//            }
         }
 
         //
@@ -190,62 +194,45 @@ namespace TipExpert.Net.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ExternalLoginConfirmation(ExternalLoginConfirmationViewModel model, string returnUrl = null)
         {
-            if (User.IsSignedIn())
-            {
-                //                return RedirectToAction(nameof(ManageController.Index), "Manage");
-                return _RedirectToLocal(".");
-            }
+            return await _Error();
 
-            if (ModelState.IsValid)
-            {
-                // Get the information about the user from the external login provider
-                var info = await _signInManager.GetExternalLoginInfoAsync();
-                if (info == null)
-                {
-                    //return View("ExternalLoginFailure");
-                    return View("Error");
-                }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await _userManager.CreateAsync(user);
-                if (result.Succeeded)
-                {
-                    result = await _userManager.AddLoginAsync(user, info);
-                    if (result.Succeeded)
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        _logger.LogInformation(6, "User created an account using {Name} provider.", info.LoginProvider);
-                        return _RedirectToLocal(returnUrl);
-                    }
-                }
-                _AddErrors(result);
-            }
-
-            ViewData["ReturnUrl"] = returnUrl;
-            return View(model);
+//            if (User.IsSignedIn())
+//            {
+//                //                return RedirectToAction(nameof(ManageController.Index), "Manage");
+//                return _RedirectToLocal(".");
+//            }
+//
+//            if (ModelState.IsValid)
+//            {
+//                // Get the information about the user from the external login provider
+//                var info = await _signInManager.GetExternalLoginInfoAsync();
+//                if (info == null)
+//                {
+//                    //return View("ExternalLoginFailure");
+//                    return View("Error");
+//                }
+//                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+//                var result = await _userManager.CreateAsync(user);
+//                if (result.Succeeded)
+//                {
+//                    result = await _userManager.AddLoginAsync(user, info);
+//                    if (result.Succeeded)
+//                    {
+//                        await _signInManager.SignInAsync(user, isPersistent: false);
+//                        _logger.LogInformation(6, "User created an account using {Name} provider.", info.LoginProvider);
+//                        return _RedirectToLocal(returnUrl);
+//                    }
+//                }
+//                _AddErrors(result);
+//            }
+//
+//            ViewData["ReturnUrl"] = returnUrl;
+//            return View(model);
         }
 
-        #endregion
-
-        #region Helpers
-
-        private void _AddErrors(IdentityResult result)
+        private Task<IActionResult> _Error()
         {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
-        }
-
-        private IActionResult _RedirectToLocal(string returnUrl)
-        {
-            if (Url.IsLocalUrl(returnUrl))
-            {
-                return Redirect(returnUrl);
-            }
-            else
-            {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
-            }
+            return Task.Run(() =>(IActionResult)HttpBadRequest("Not implemented!"));
         }
 
         #endregion
