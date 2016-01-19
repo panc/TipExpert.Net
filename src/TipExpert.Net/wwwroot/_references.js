3e802ecd4546f6e91fb8d3fa615632a36f118835
Christoph Pangerl <christoph.pangerl@gmail.com>
2016-01-19 22:24:16 +0100

    Corrected footer to stick at the bottom...

--- src/TipExpert.Net/wwwroot/_references.js
+++ src/TipExpert.Net/wwwroot/_references.js
@@ -33,5 +33,4 @@
 /// <reference path="lib/angular-ui-router/release/angular-ui-router.js" />
 /// <reference path="lib/angular-ui-utils/ui-utils.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/hammer.js/hammer.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
